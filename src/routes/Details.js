442bbc34f7b964992fac7c1c19a4c75ec2a4a00f
sooyeon <qkrtndus3062@naver.com>
2021-07-07 09:08:20 +0900

    21.07.06

---
+++ src/routes/Details.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Details extends React.Component {
+    componentDidMount() {
+        const { location, history } = this.props;
+    }
+}
+
+export default Details;
